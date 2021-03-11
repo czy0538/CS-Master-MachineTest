d19dc7f655c98a2525a79796c54c5724da78a31b
Zhiyuan Cao <31185624+czy0538@users.noreply.github.com>
2021-03-11 10:29:50 +0800

    sort整理

--- test.cpp
+++ test.cpp
@@ -3,15 +3,40 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <algorithm>
 using LL = long long;
 using namespace std;
-int isLeapYear(const int &year)
+
+class Stu
 {
-    return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0) ? 1 : 0;
-}
+public:
+    int id;
+    string name;
+    Stu(int iid, string iname) : id(iid), name(iname) {}
+    bool operator()(const Stu &i,const Stu &j)
+    {
+        return i.id < j.id;
+    }
+    bool operator <(const Stu &i)
+    {
+        return id < i.id;
+    }
+}Cmp(0,"0");
 int main()
 {
-    int year = 2021;
-    cout << isLeapYear(year) << endl;
+    vector<Stu> stus;
+    stus.push_back(Stu(10, "10"));
+    stus.push_back(Stu(1, "1"));
+    for (const auto &j : stus)
+    {
+        cout << j.id << endl;
+    }
+    //sort(stus.begin(), stus.end(), Cmp);
+
+    sort(stus.begin(), stus.end());
+    for(const auto &j:stus)
+    {
+        cout << j.id << endl;
+    }
     return 0;
 }
